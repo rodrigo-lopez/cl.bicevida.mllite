3189a77261b28b9dc7c596d0e51df65c9647513b
rodrigo-lopez <rodrigo.lopez@bicevida.cl>
2014-03-21 11:43:03 -0300

    Se reemplaza localhost por 127.0.0.1
    Sólo para efectos de prueba

--- ViewController/public_html/yeoman/app/scripts/services/services.js
+++ ViewController/public_html/yeoman/app/scripts/services/services.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('mlliteApp').factory('BuscaPolizaPorTitular', function ($resource, $log) {
-  return $resource('http://localhost:7101/ServiciosColectivos/jersey/polizas/titular/:rutTitular', null, {
+  return $resource('http://127.0.0.1:7101/ServiciosColectivos/jersey/polizas/titular/:rutTitular', null, {
     'consultar':   {method:'GET', params:{rutTitular: null}, isArray: false}
   });
 });
@@ -9,14 +9,14 @@ angular.module('mlliteApp').factory('BuscaPolizaPorTitular', function ($resource
 
 
 angular.module('mlliteApp').factory('BuscarGrupoFamiliar', function ($resource, $log) {
-  return $resource('http://localhost:7101/ServiciosColectivos/jersey/polizas/grupofamiliar/:numeroPoliza/:rutTitular', null, {
+  return $resource('http://127.0.0.1:7101/ServiciosColectivos/jersey/polizas/grupofamiliar/:numeroPoliza/:rutTitular', null, {
     'consultar':   {method:'GET', params:{numeroPoliza: null, rutTitular: null}, isArray: true}
   });
 });
 
 
 angular.module('mlliteApp').factory('BuscarPlanPrestacionPorGrupoTipoBeneficiario', function ($resource, $log) {
-  return $resource('http://localhost:7101/ServiciosColectivos/jersey/polizas/planprestacion/:numeroPoliza/:numeroGrupo/:tipoBeneficiario', null, {
+  return $resource('http://127.0.0.1:7101/ServiciosColectivos/jersey/polizas/planprestacion/:numeroPoliza/:numeroGrupo/:tipoBeneficiario', null, {
     'consultar':   {method:'GET', params:{numeroPoliza: null, numeroGrupo: null, tipoBeneficiario: null}, isArray: true}
   });
 });

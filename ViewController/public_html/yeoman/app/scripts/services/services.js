2e9d061fc2bd0e0c4a9dac6cd0358e29a96a3952
rodrigo-lopez <rodrigo.lopez@bicevida.cl>
2014-04-08 15:39:20 -0300

    Se cambian URL absolutas en llamadas a servicios REST por URL relativas

--- ViewController/public_html/yeoman/app/scripts/services/services.js
+++ ViewController/public_html/yeoman/app/scripts/services/services.js
@@ -1,19 +1,19 @@
 'use strict';
 
 angular.module('mlliteApp').factory('BuscaPolizaPorTitular', function ($resource, $log) {
-  return $resource('http://127.0.0.1:7101/ServiciosColectivos/jersey/polizas/titular/:rutAsegurado/:fechaAtencion/:rutUsuario', null, {
+  return $resource('/sec/sc/ServiciosColectivos/jersey/polizas/titular/:rutAsegurado/:fechaAtencion/:rutUsuario', null, {
     'consultar':   {method:'GET', params:{rutAsegurado: null, fechaAtencion:null, rutUsuario:null}, isArray: false}
   });
 });
 
 angular.module('mlliteApp').factory('BuscarGrupoFamiliar', function ($resource, $log) {
-  return $resource('http://127.0.0.1:7101/ServiciosColectivos/jersey/polizas/grupofamiliar/:numeroPoliza/:rutAsegurado/:fechaAtencion', null, {
+  return $resource('/sec/sc/ServiciosColectivos/jersey/polizas/grupofamiliar/:numeroPoliza/:rutAsegurado/:fechaAtencion', null, {
     'consultar':   {method:'GET', params:{numeroPoliza: null, rutAsegurado: null, fechaAtencion:null}, isArray: true}
   });
 });
 
 angular.module('mlliteApp').factory('BuscarPlanPrestacionPorGrupoTipoBeneficiario', function ($resource, $log) {
-  return $resource('http://127.0.0.1:7101/ServiciosColectivos/jersey/polizas/planprestacion/:numeroPoliza/:numeroGrupo/:numeroAsegurado/:numeroCarga', null, {
+  return $resource('/sec/sc/ServiciosColectivos/jersey/polizas/planprestacion/:numeroPoliza/:numeroGrupo/:numeroAsegurado/:numeroCarga', null, {
     'consultar':   {method:'GET', params:{numeroPoliza: null, numeroGrupo: null, numeroAsegurado: null, numeroCarga: null}, isArray: true}
   });
 });
